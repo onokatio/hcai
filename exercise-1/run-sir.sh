163381427ce16cd88bde478e4fac82759636c054
onokatio <onokatio@gmail.com>
2024-11-12 17:11:22 +0900

    fix

--- exercise-1/run-sir.sh
+++ exercise-1/run-sir.sh
@@ -4,5 +4,5 @@ for i in `seq 1 100`
 do
    python3 sir_random.py $net >> random/${net}_result
 done
-cat random/${net}_result |cut -f 0,5 -d " "| python3 calc_ave.py 
+cat random/${net}_result |cut -f 1,5 -d " "| python3 calc_ave.py 
 
